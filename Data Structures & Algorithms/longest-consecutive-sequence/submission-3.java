8105eba63726bf513e80110c00b62fb1203e9674
Sidharth Singh <111000492+sid563@users.noreply.github.com>
2026-05-27 21:26:01 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+        // Arrays.sort(nums);
+        
+        // int longest = 1;
+        // int current = 1;
+        // int num =nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==num){
+        //          continue;
+        //     }else if(nums[i]>num+1){
+        //         current=1;
+        //         num=nums[i];
+        //     }else{
+        //         current++;
+        //         num=nums[i];
+        //         if(current>longest) {
+        //             longest=current;
+        //         }
+        //     }
+        // }
+
+        //Optimal Solution
+        // HashSet<Integer> set = new HashSet<>();
+        // for(int i:nums){
+        //     set.add(i);
+        // }
+        // int longest = 1;
+        // for(int num:set){
+        //     if(!set.contains(num-1)){
+        //         int strike = 1;
+        //         while(set.contains(num+strike)){
+        //             strike++;
+        //         }
+        //         longest=Math.max(strike,longest);
+        //     }
+        // }
+        HashSet <Integer> set = new HashSet <>();
+        int result = 0;
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        for(int num : set){
+            if(!set.contains(num - 1)){//start counting from here
+                int streak = 1;
+                while(set.contains(num + streak)){
+                    streak ++;
+                }
+                result = Math.max(streak , result);
+            }
+        }
+        return result;
+
+
+        //return longest;
+    }
+}
